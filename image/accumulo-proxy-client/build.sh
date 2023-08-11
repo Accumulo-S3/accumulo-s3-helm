5815b6b709a949b2842c7fc4a5a3aaaa63938d3e
Chris Bevard <cbevard1@gmail.com>
2023-08-11 14:14:33 -0400

    added prestop hook to tservers

--- image/accumulo-proxy-client/build.sh
+++ image/accumulo-proxy-client/build.sh
@@ -1,6 +1,6 @@
 tag=$1
 repo=$2
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$repo"
-docker build --no-cache -t accumulo-s3:"$tag" .
-docker tag accumulo-s3:"$tag"   "$repo"/accumulo-s3:"$tag"
-docker push  "$repo"/accumulo-s3:"$tag"
+docker build --no-cache -t accumulo-proxy-client:"$tag" .
+docker tag accumulo-s3:"$tag"   "$repo"/accumulo-proxy-client:"$tag"
+docker push  "$repo"/accumulo-proxy-client:"$tag"
